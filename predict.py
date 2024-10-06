1fe088427c7c7669bb45f87ade7689cec00771bb
Malhar Ujawane <Malharujawane@gmail.com>
2024-10-06 12:19:05 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,14 +7,14 @@ import torch
 from PIL import Image
 from transformers import MllamaForConditionalGeneration, AutoProcessor
 
-model_id = "unsloth/Llama-3.2-11B-Vision-Instruct"
+model_id = "unsloth/Llama-3.2-90B-Vision-Instruct"
 
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""      
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'  
-        self.model = MllamaForConditionalGeneration.from_pretrained(
+        self.model = model = MllamaForConditionalGeneration.from_pretrained(
             model_id,
             torch_dtype=torch.bfloat16,
             device_map="auto",
@@ -23,9 +23,14 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-       image: Path = Input(description="Input image"),
+        image: Path = Input(description="Input image"),
         prompt: str = Input(description="Text prompt to send to the model."),
-        max_new_tokens: int = Input(description="Maximum number of tokens to generate. A word is generally 2-3 tokens.")
+        temperature: float = Input(description="Controls randomness. Lower values make the model more deterministic, higher values make it more random.", default=0.7, ge=0.0, le=1.0),
+        top_p: float = Input(description="Controls diversity of the output. Lower values make the output more focused, higher values make it more diverse.", default=0.95, ge=0.0, le=1.0),
+        max_new_tokens: int = Input(
+            description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
+            ge=1,
+            default=512
     ) -> Path:
         """Run a single prediction on the model"""
         image = Image.open(image).convert('RGB')
@@ -33,7 +38,7 @@ class Predictor(BasePredictor):
         messages = [
             {"role": "user", "content": [
                 {"type": "image"},
-                {"type": "text", "text": prompt}
+                {"type": "text", "text": "If I had to write a haiku for this one, it would be: "}
             ]}
         ]
         input_text = self.processor.apply_chat_template(messages, add_generation_prompt=True)
@@ -42,8 +47,8 @@ class Predictor(BasePredictor):
             input_text,
             add_special_tokens=False,
             return_tensors="pt",
-        ).to(model.device)
+        ).to(self.model.device)
 
-        output = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
+        output = self.model.generate(**inputs, temperature=temperature, top_p=top_p, max_new_tokens=max_new_tokens)
         return [self.processor.decode(output, skip_special_tokens=True) for output in outputs]
 
